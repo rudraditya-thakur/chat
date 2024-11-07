74611a423efcc2e9cc82433d88c8007b80182a72
Akshat Upadhyay <upadhyay.akshat29@gmail.com>
2024-11-07 09:43:48 +0530

    db added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-// "use client";
-// import { useQuery } from "convex/react";
-// import { api } from "@/convex/_generated/api";
+"use client";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 import Slider from "@/components/Slider";
 import SuggestedProductSection from "@/components/SuggestedProductSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import { products } from "@/data/products";
 
 export default function Home() {
+  const products = useQuery(api.products.get);
   console.log(products);
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
